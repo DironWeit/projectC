952f1a3bad59f0a0af8097dddcb053fdad425789
UbuntoMyPC <Antonoff2007@yandex.ru>
2026-03-09 16:18:55 +0300

    V 0.0.3

--- logic/logic.c
+++ logic/logic.c
@@ -32,15 +32,35 @@ int pokazPole(int pole[4][4]) {
 //инициализируем переменные и рандомные числа
 //если число процента юольше 90 то подставим число 4, если нет, то 2
 //и заполним нужную клетку числои
+//функция рандома измененена на более рандомную
 int addRandTitle(int pole[4][4]) {
-  int i = rand() % 3 + 0;
-  int j = rand() % 3 + 0;
+  int i = rand() % 100;
+  int j = rand() % 100;
+
+  if (i >= 0 && i <25) {
+    i = 1;
+  } else if (i >= 25 && i < 50) {
+    i = 2;
+  } else if (i >=50 && i < 75) {
+    i = 3;
+  } else {
+    i =4;
+  }
+  if (j >= 0 && j <25) {
+    j = 1;
+  } else if (j >= 25 && j < 50) {
+    j = 2;
+  } else if (j >=50 && j < 75) {
+    j = 3;
+  } else {
+    j =4;
+  }
+
   int proc = rand() % 100 + 1;
   int num;
   if (pole[i][j] != 0) {
     return addRandTitle(pole);
   }
-  
 
   if (proc >=90) {
     num = 4;
@@ -50,6 +70,8 @@ int addRandTitle(int pole[4][4]) {
   pole[i][j] = num;
   return 0;
 }
+
+
 
 
 
